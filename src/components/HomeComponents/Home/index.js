0dd30775e65030a6ac2e7d694a5d95ef0b049aa0
edos009 <dostyutyunnik@gmail.com>
2022-11-05 20:44:34 +0200

    Add styles for btn go on home page

--- src/components/HomeComponents/Home/index.js
+++ src/components/HomeComponents/Home/index.js
@@ -1,15 +1,17 @@
 import React from "react";
 import cx from "classnames";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 import CONSTANTS from "../../../constants";
 
 import styles from "./Home.module.scss";
-import { useSelector } from "react-redux";
 
 const { THEMES } = CONSTANTS;
 
 const Home = () => {
   const themes = useSelector((state) => state.themes.themes);
+  const navigateCountry = useNavigate();
 
   const stylesHome = cx(
     styles.home,
@@ -22,14 +24,31 @@ const Home = () => {
     }
   );
 
+  const stylesCountriesBtnGo = cx(
+    styles.home_btn_go,
+    {
+      [styles.color_dark_theme]: themes === THEMES.LIGHT,
+      [styles.color_light_theme]: themes === THEMES.DARK,
+    },
+    {
+      [styles.bg_light_theme]: themes === THEMES.LIGHT,
+      [styles.bg_dark_theme]: themes === THEMES.DARK,
+    }
+  );
+
   return (
     <section className={stylesHome}>
       <div className={styles.container}>
         <div className={styles.home_inner}>
           <h1 className={styles.home_title}>
-            This is our countries application
+            This is our countries app
           </h1>
-          <button className={styles.home_btn_go}>Let's go</button>
+          <button
+            className={stylesCountriesBtnGo}
+            onClick={() => navigateCountry(`/countries`)}
+          >
+            Let's go
+          </button>
         </div>
       </div>
     </section>
