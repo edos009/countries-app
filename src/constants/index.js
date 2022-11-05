d232522fe76b4369fa8e7927fc1522826a8d19dd
edos009 <dostyutyunnik@gmail.com>
2022-11-05 17:47:43 +0200

    Add constant values

---
+++ src/constants/index.js
@@ -0,0 +1,14 @@
+const CONSTANTS = {
+  THEMES: {
+    LIGHT: "LIGHT",
+    DARK: "DARK",
+  },
+  NAVIGATION: {
+    LINKS: [
+      { id: 1, title: "home", to: "/" },
+      { id: 2, title: "countries", to: "/countries" },
+    ],
+  },
+};
+
+export default CONSTANTS;
